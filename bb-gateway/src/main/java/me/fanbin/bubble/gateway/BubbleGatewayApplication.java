de6880909903f63b8048f635a34c568ac5e316c5
Fan Bin <fanbin0602@163.com>
2021-04-21 15:53:42 +0800

    添加 bb-gateway 工程

---
+++ bb-gateway/src/main/java/me/fanbin/bubble/gateway/BubbleGatewayApplication.java
@@ -0,0 +1,17 @@
+package me.fanbin.bubble.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fanbin
+ * @date 2021/4/21
+ */
+@SpringBootApplication
+public class BubbleGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BubbleGatewayApplication.class, args);
+    }
+
+}
