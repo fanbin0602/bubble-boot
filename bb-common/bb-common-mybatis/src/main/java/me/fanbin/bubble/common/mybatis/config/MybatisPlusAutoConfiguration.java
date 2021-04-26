ae00694c19c0bb1bf4dc39334452504f6cb49f02
Fan Bin <fanbin0602@163.com>
2021-04-26 14:34:31 +0800

    bb-common-mybatis 添加 MybatisPlus 配置

---
+++ bb-common/bb-common-mybatis/src/main/java/me/fanbin/bubble/common/mybatis/config/MybatisPlusAutoConfiguration.java
@@ -0,0 +1,27 @@
+package me.fanbin.bubble.common.mybatis.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author fanbin
+ * @date 2021/4/26
+ */
+@Configuration
+public class MybatisPlusAutoConfiguration {
+
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
