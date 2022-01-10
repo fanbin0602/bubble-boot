619fda383e910cbc626f8605003afea9f6c409cd
Fan Bin <fanbin0602@163.com>
2022-01-10 09:31:21 +0800

    更新服务接口命名

---
+++ bb-urpm/bb-urpm-biz/src/main/java/me/fanbin/bubble/urpm/biz/service/DeptService.java
@@ -0,0 +1,30 @@
+package me.fanbin.bubble.urpm.biz.service;
+
+import me.fanbin.bubble.urpm.api.model.SysDept;
+import me.fanbin.bubble.urpm.api.service.IDeptService;
+
+/**
+ * @author fanbin
+ * @date 2021/4/26
+ */
+public class DeptService implements IDeptService {
+    @Override
+    public void add(SysDept dept) {
+
+    }
+
+    @Override
+    public void update(SysDept dept) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public SysDept getById(Integer id) {
+        return null;
+    }
+}
